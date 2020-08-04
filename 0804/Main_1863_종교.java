7579d755dc81515e126f6f9797782a960e5eb87b
skeldtcan <skeldtcan@daum.net>
2020-08-05 08:57:44 +0900

    0804 HW

--- 0804/Main_1863_종교.java
+++ 0804/Main_1863_종교.java
@@ -1,36 +1,59 @@
-package com.ssafy.java.day14;
+package com.ssafy.java.day15;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
 import java.util.*;
 
 public class Main_1863_종교 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer tokens;
-	static StringBuilder output = new StringBuilder();
-	static int N, K;
-	
-	public static void main(String[] args) throws Exception{
-		tokens = new StringTokenizer(input.readLine());
-		Queue<Integer> llist = new LinkedList<Integer>();
-		N = Integer.parseInt(tokens.nextToken());
-		K = Integer.parseInt(tokens.nextToken());
-		for(int i=1; i<=N; i++) {
-			llist.add(i);
-		}
-		
-		output.append("<");
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<K-1; j++) {
-				llist.add(llist.poll());
-			}
-			output.append(llist.poll()).append(", ");
-		}
-		output.delete(output.length() - 2, output.length()).append(">");
-		System.out.println(output);
-		
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+    
+    static int N;
+    static int M;
+    static int[] repres;
+    
+	public static void main(String[] args) throws Exception {
+        tokens = new StringTokenizer(input.readLine());
+        N = Integer.parseInt(tokens.nextToken());
+        M = Integer.parseInt(tokens.nextToken());
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        repres = new int[N + 1];
+        for (int i = 1; i < repres.length; i++)
+        	repres[i] = i;
+        	
+        for (int m = 0; m < M; m++) {
+            tokens = new StringTokenizer(input.readLine());
+            int a = Integer.parseInt(tokens.nextToken());
+            int b = Integer.parseInt(tokens.nextToken());
+            union(a, b);
+        }
+        for(int i=1; i<N+1; i++) {
+        	repres[i] = findSet(i);
+        }
+        for(int a: repres)
+        	set.add(a);
+        System.out.println(set.size()-1);
+            
 	}
+	static void union(int a, int b) {
+        a = findSet(a);
+        b = findSet(b);
 
+        if (a < b) {
+            repres[b] = a;
+        } else {
+            repres[a] = b;
+        }
+    }
+	
+    static int findSet(int v) {
+        if (repres[v] == v) {
+            return v;
+        }
+        else {
+            return repres[v] = findSet(repres[v]);
+        }
+    }
+    
 }
