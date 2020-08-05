4655d6f7f593bfe33288ced0ec1051a5be40a001
class7 <class7@M800>
2020-08-05 17:59:14 +0900

    0805 HW

--- 0805/Main_2667_단지번호붙이기.java
+++ 0805/Main_2667_단지번호붙이기.java
@@ -1,52 +1,69 @@
-package com.ssafy.java.day11;
+package com.ssafy.java.day0805;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main_2667_단지번호붙이기 {
-    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static int N, Group, Cnt, Answer=0;
+    static BufferedReader input;
     static StringBuilder output = new StringBuilder();
-    static int N;
+    static StringTokenizer tokens;
+    static int[][] map;
+    static int[][] group;
+    static int[] row = {-1, 1, 0, 0};
+    static int[] col = {0, 0, -1, 1};
     
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        N = Integer.parseInt(bf.readLine());
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        
-        Stack<Top> stack = new Stack<Top>();
-        for (int i = 0; i < N; i++) {
-            int building = Integer.parseInt(st.nextToken());
-            while (!stack.isEmpty()) {
-                if (stack.peek().height <= building) {
-                    stack.pop();
-                } else {
-                    break;
-                }
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        input = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(input.readLine());
+        List<Integer> list = new ArrayList<Integer>();
+        Group = 1;
+
+        map = new int [N+2][N+2];
+        for(int[] a : map)
+            Arrays.fill(a, -1);
+        group = new int [N+2][N+2];
+        for(int r=1; r<=N; r++) {
+            String line = (input.readLine());
+            for(int c=1; c<=N; c++) {
+               map[r][c] = (int)(line.charAt(c-1)-'0'); 
             }
-            if(stack.isEmpty())
-                output.append("0 ");
-            else {
-                output.append(stack.peek().idx + 1+ " ");
-            }
-            stack.push(new Top(building, i));
         }
-        System.out.print(output);
+        
+
+        for(int r=1; r<=N; r++) {
+            for(int c=1; c<=N; c++) {
+               if(map[r][c] == 1 && group[r][c] == 0) {
+                   Cnt = 0;
+                   findLand(r, c);
+                   Group++;
+                   list.add(Cnt);
+               }
+            }
+        }
+        
+        list.sort(null);
+        System.out.println(Group-1);
+        for(Integer i: list)
+            System.out.println(i);
+    }
+    static void findLand(int r, int c) {
+        if (group[r][c] != 0)
+            return;
+        Cnt++;
+        group[r][c] = Group;
+        for(int i=0; i<4; i++) {
+            int r2 = r+row[i];
+            int c2 = c+col[i];
+            if(map[r2][c2] == 1 && group[r2][c2] == 0)
+                findLand(r2, c2);
+        }
     }
     
-    static class Top {
-        int height;
-        int idx;
-        
-        public Top(int height, int idx) {
-            this.height = height;
-            this.idx = idx;
-        }
-        
-        @Override
-        public String toString() {
-            return "[" + height + "," + idx + "]";
-        }
-    }
 }
