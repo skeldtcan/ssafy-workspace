472c0b9e782c7c3f299b43e98047605ac9aa8c2f
skeldtcan <skeldtcan@daum.net>
2020-08-30 09:52:03 +0900

    0828 HW

--- 0828/Main_15961_회전_초밥.java
+++ 0828/Main_15961_회전_초밥.java
@@ -1,85 +1,76 @@
-package com.ssafy.java.day0827;
+package com.ssafy.java.day0828;
 
 import java.io.*;
 import java.util.*;
 
-//빵집 파이프문제
+
 public class Main_15961_회전_초밥 {
-    static int R, C, K, Answer = 0;
+	static int N, D, K, C, Answer=0;
     static BufferedReader input;
-    static StringBuilder output = new StringBuilder();
     static StringTokenizer tokens;
-    static char[][] map;
-    static int[][] dirs = {{1,1}, {0,1}, {-1,1}};
-    static boolean[][] visited;
-    static Stack<Pos> route = new Stack<>();
+    static int[] arr;
+    static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws Exception{
-        System.setIn(new FileInputStream("input.txt"));
         input = new BufferedReader(new InputStreamReader(System.in));
         tokens = new StringTokenizer(input.readLine());
       
-        R = Integer.parseInt(tokens.nextToken());
+        N = Integer.parseInt(tokens.nextToken());
+        D = Integer.parseInt(tokens.nextToken());
+        K = Integer.parseInt(tokens.nextToken());
         C = Integer.parseInt(tokens.nextToken());
-        map = new char[R][C];
-        visited = new boolean[R][C];
-       
-        for(int r=0; r<R; r++) {
-            map[r] = input.readLine().toCharArray();
+        arr = new int[N+K];
+        int cnt = 0;
+        
+        for(int n=0; n<N; n++) {
+        	arr[n] = Integer.parseInt(input.readLine());
         }
-       
-        for(int r=0; r<R; r++) {
-        	route.push(new Pos(r, 0, new Pos[C]));
-        	while(!route.isEmpty())
-        		find(route.pop());
+        //System.out.println(Arrays.toString(arr));
+        
+        for(int i=0; i<K; i++) {
+        	if(map.containsKey(arr[i])) {
+        		map.put(arr[i], map.get(arr[i])+1);
+        	} else {
+        		map.put(arr[i], 1);
+        	}
+        }
+        // 보너스 초밥
+        if (!map.containsKey(C))
+        	cnt = map.size() + 1;
+        else
+        	cnt = map.size();
+        
+        //System.out.println(cnt+", " + map.keySet());
+        Answer = Math.max(Answer, cnt);
+        
+        for(int i=K; i<N+K; i++) {
+        	if (Answer == K + 1)
+        		break;
+        	int re = i-K;
+        	int idx = i%N;
+        	cnt = 0;
+        	
+    		// 초밥 제거
+    		if (map.get(arr[re]) == 1)
+    			map.remove(arr[re]);
+    		else
+    			map.put(arr[re], map.get(arr[re])-1);
+    		
+    		// 초밥 추가
+    		if (map.containsKey(arr[idx]))
+    			map.put(arr[idx], map.get(arr[idx])+1);
+    		else
+    			map.put(arr[idx], 1);
+    		
+    		// 보너스 초밥
+    		if (!map.containsKey(C))
+            	cnt = map.size() + 1;
+            else
+            	cnt = map.size();
+    		
+        	//System.out.println(cnt+", " + map.keySet());
+        	Answer = Math.max(Answer, cnt);
         }
         System.out.println(Answer);
-    }
-   
-    static void find(Pos pos) {
-    	boolean chk = false;
-    	System.out.println(pos.r + ", " + pos.c);
-    	int r = pos.r;
-    	int c = pos.c;
-    	Pos[] trace = pos.trace;
-    	trace[c] = new Pos(r, c, null);
-    	
-    	if (pos.c == C-1) {
-    		Answer++;
-    		for(Pos a: trace)
-    			visited[a.r][a.c] = true; 
-    		route.clear();
-    	}
-    		
-    	
-    	for(int i=0; i<3; i++) {
-    		int nr = r + dirs[i][0];
-    		int nc = c + dirs[i][1];
-    		if (isIn(nr, nc) && map[nr][nc] == '.' && !visited[nr][nc]) {
-    			route.add(new Pos(nr, nc, trace));
-    			chk = true;
-    		}
-    	}
-    }
-   
-    static class Pos {
-    	int r, c;
-    	Pos[] trace;
-	   
-		public Pos(int r, int c, Pos[] trace) {
-		super();
-		this.r = r;
-		this.c = c;
-		this.trace = trace;
-		}
-		
-		@Override
-        public String toString() {
-            return "[" + r + "," + c + "]";
-        }
-	}
-    
-    static boolean isIn(int r, int c) {
-    	return r>=0 && r<R && c>=0 && c<C;
     }
 }
