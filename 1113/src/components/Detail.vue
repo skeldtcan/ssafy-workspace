af387f599c215fd68b51c5de5ff84c86da87aeba
skeldtcan <skeldtcan@daum.net>
2020-11-13 17:00:44 +0900

    1113 list 구현

---
+++ 1113/src/components/Detail.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+        <h2 class="text-center">Vue를 이용한 게시판</h2>
+        <table class="table table-condensed w-25">
+        <tr>
+            <th>아이디</th>
+            <td v-text="board.id"></td>
+        </tr>
+        <tr>
+            <th>이름</th>
+            <td v-text="board.name"></td>
+        </tr>
+        <tr>
+            <th>부서</th>
+            <td v-text="board.dept_id"></td>
+        </tr>
+        <tr>
+            <th>직급</th>
+            <td v-text="board.title"></td>
+        </tr>
+        <tr>
+            <th>월급</th>
+            <td v-text="board.salary"></td>
+        </tr>
+        <tr>
+            <td colspan="2" v-text="board.content"></td>
+        </tr>
+        </table>
+        <div  class='text-right'>
+            <router-link  class='btn btn-primary' to='/list'>목록</router-link>
+            <router-link  class='btn btn-primary' :to="'/update?id='+board.id">수정</router-link>
+            <router-link  class='btn btn-primary' :to="'/delete?id='+board.id">삭제</router-link>
+        </div>
+    </div>
+</template>
+<script>
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      board: {}
+    };
+  },
+  created() {
+      const params = new URL(document.location).searchParams;
+      axios.get(`http://localhost:8097/hrmboot/api/employee/all/${params.get('id')}`)
+          .then(({ data }) => {
+              this.board = data
+          })
+          .catch(error => {
+            alert(error);
+          });
+  },
+  name:`detail`
+}
+</script>
+
+  
---
+++ 1113/src/components/List.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+      <h2 class="text-center">Vue를 이용한 게시판</h2>
+      <div class="text-right">
+        <button class="btn btn-primary" @click="movePage">등록</button>
+      </div>
+      <div v-if="boards.length">
+        <div class="text-center">
+          <input type="text" v-model="sname" />
+          <button @click="searchName">검색</button>
+        </div>
+        <table class="table table-boarded table-condensed">
+          <tr>
+            <td>아이디</td>
+            <td>이름</td>
+            <td>부서</td>
+            <td>직책</td>
+            <td>월급</td>
+          </tr>
+          <tr v-for="(board, index) in boards" :key="index">
+            <td>
+              <router-link :to="'detail?id='+board.id"
+                >{{board.id}}</router-link
+              >
+            </td>
+            <td v-text="board.name"></td>
+            <td v-text="board.dept_id"></td>
+            <td v-text="board.title"></td>
+            <td v-text="board.salary"></td>
+          </tr>
+        </table>
+      </div>
+      <div v-else>
+        <h3 class="text-center">게시글이 없습니다</h3>
+      </div>
+    </div>
+</template>
+<script>
+
+import axios from "axios";
+
+    export default {
+        data() {
+    return {
+      boards: [], //화면에 표시할 board
+      src: [], //localStorage에서 추출한 board 원본
+      sname: ""
+    };
+  },
+  methods: {
+    movePage: function() {
+      this.$router.push("/create");
+    },
+    searchName: function() {
+      if (this.sname != "") {
+        // this.boards = this.src.filter( board =>{
+        //     return this.sname == board.writer
+        // })
+        const temp = [];
+        for (let board of this.src) {
+          if (this.sname == board.writer) {
+            temp.push(board);
+          }
+        }
+        this.boards = temp;
+      } else {
+        this.boards = this.src;
+      }
+    }
+  },
+  created() {
+    axios
+      .get("http://localhost:8097/hrmboot/api/employee/all")
+      .then(response => {
+        this.src = response.data;
+        this.boards = response.data;
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+}
+</script>
+
+  
